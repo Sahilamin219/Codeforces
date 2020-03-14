3eed864940fb7b979f5ca3e23836b980de80b063
Sahil Amin <sahilamin219@gmail.com>
2020-03-14 20:42:00 +0530

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/73246063

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <stack>
+#include <limits>
+#include <bits/stdc++.h>
+#define n(a) int(a.size())
+// #define SZ(x)                   ((int)x.size())
+#define fori(i, a, b) for (int i = a; i < b; i++)
+#define veti vector <pair<int,int>> v
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        cout<<n-1<<" "<<1<<endl;
+    }
+    return 0;
+}
