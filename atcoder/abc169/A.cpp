a85864e7de8065534b7cd65f07d77bc28b2737a4
Sahil Amin <sahilamin219@gmail.com>
2020-05-31 17:34:00 +0530

    Add solution for problem `A - Multiplication 1` Link: https://atcoder.jp/contests/abc169/tasks/abc169_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc169/submissions/13785799

---
+++ atcoder/abc169/A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+int main(){
+  	fast;
+    int n,k;cin>>n>>k;
+    cout<<k*n;
+    return 0;
+}
