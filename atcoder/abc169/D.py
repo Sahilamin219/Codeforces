58f49e7832ad89e44c9854d3b51d56e7b3990ea2
Sahil Amin <sahilamin219@gmail.com>
2020-06-02 20:55:00 +0530

    Add solution for problem `D - Div Game` Link: https://atcoder.jp/contests/abc169/tasks/abc169_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc169/submissions/13961057

--- atcoder/abc169/D.py
+++ atcoder/abc169/D.py
@@ -1,5 +1,8 @@
 n=int(input())
 ans=0
+#it is the power of every prime number
+#and as z is always a prime numer we should not bother about
+# combiations of prime numer dividing N
 for i in range(2,1000010):
 	power=0
 	j=1
