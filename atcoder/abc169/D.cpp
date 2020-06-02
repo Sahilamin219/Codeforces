c4763a15ddd53da17c1480d20484a826a4a8ed3f
Sahil Amin <sahilamin219@gmail.com>
2020-06-02 20:18:00 +0530

    Add solution for problem `D - Div Game` Link: https://atcoder.jp/contests/abc169/tasks/abc169_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc169/submissions/13959939

--- atcoder/abc169/D.cpp
+++ atcoder/abc169/D.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef long double ld;
 #define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
 int main() {
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0); 
     ll N; cin >> N;
     int ans = 0;
     FOR(i, 2, 100000000) {
@@ -18,6 +22,6 @@ int main() {
         }
     }
     if (N > 1) ans++;
-    cout << ans << "\n";
+    cout << ans << endl;
     return 0;
 }
