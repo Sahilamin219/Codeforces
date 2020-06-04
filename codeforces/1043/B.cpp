b668cc969442dfab524f0c1e10fbb686b6d0ac9d
Sahil Amin <sahilamin219@gmail.com>
2020-06-04 07:24:00 +0530

    Add solution for problem `B - Lost Array` Link: https://codeforces.com/contest/1043/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/1043/submission/82441778

--- codeforces/1043/B.cpp
+++ codeforces/1043/B.cpp
@@ -3,6 +3,10 @@
 #define r(i,a,b) for(int i=a;i>b;i--) 
 using namespace std;
 int main(){
+	 #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int n;
     cin>>n;
