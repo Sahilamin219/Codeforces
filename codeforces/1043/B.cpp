dd40ff4f7c6597f06a92f5dab16f61c912925e0c
Sahil Amin <sahilamin219@gmail.com>
2020-06-04 07:25:00 +0530

    Add solution for problem `B - Lost Array` Link: https://codeforces.com/contest/1043/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/1043/submission/82441812

---
+++ codeforces/1043/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define f(i,a,b) for(int i=a;i<b;i++)
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n;
+    cin>>n;
+    vector<int> v(n,0);
+    f(i,0,n)cin>>v[i];
+    vector<int> d(n,0);
+    d[0]=v[0];
+    int maxi=d[0];
+    f(i,1,n){
+    	d[i]=v[i]-v[i-1];
+    	if(maxi<d[i])maxi=d[i];
+    }
+    int ans=1;
+   	vector<int> for_ans;
+   	for_ans.push_back(n);
+   	f(i,1,n){
+   		int flag=1;
+   		f(x,0,n){
+   			if(d[x]==d[(x+i)%i]){
+   				continue;
+   			}
+   			else{
+   				flag=0;break;
+   			}
+   		}
+   		if(flag){
+   			ans++;
+   			for_ans.push_back(i);
+   		}
+   	}
+   	cout<<ans<<"\n";
+   	sort(for_ans.begin(), for_ans.end());
+   	f(i,0,for_ans.size()){
+   		cout<<for_ans[i]<<" ";
+   	}
+	return 0;
+}
