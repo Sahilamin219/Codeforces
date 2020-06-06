ec07b808faefb9a2dbab89862bb26618fb32f84c
Sahil Amin <sahilamin219@gmail.com>
2020-06-06 20:43:00 +0530

    Add solution for problem `D - Constant Palindrome Sum` Link: https://codeforces.com/contest/1343/problem/D Tags: brute force, data structures, greedy, two pointers, *1700 Ref: https://codeforces.com/contest/1343/submission/82716501

--- codeforces/1343/D.cpp
+++ codeforces/1343/D.cpp
@@ -1,7 +1,39 @@
 #include <bits/stdc++.h>
+#define s(a) int(a.size())
 #define f(i,a,b) for(int i=a;i<b;i++)
+#define r(i,a,b) for(long long int i=a;i>b;i--) 
+#define pb push_back
+#define mkp make_pair
+#define mod 1000000007;
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
 using namespace std;
+typedef long long int ll;
+#define INF 0x3f3f3f3f;
+long long maximum=numeric_limits<int>:: max();
+long long minimum=numeric_limits<int>:: min();
+long long int countSetBits(long long int n) 
+{ 
+    long long int count = 0; 
+    while (n > 0) 
+    { 
+        count++; 
+        n &= (n-1); 
+    } 
+    return count; 
+} 
+  
+// Function that return count of flipped number 
+long long int FlippedCount(long long int a, long long int b) 
+{ 
+    // Return count of set bits in a XOR b 
+    return countSetBits(a^b); 
+} 
+
 int main(){
+	 #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int T;cin>>T;
     while(T--){
