3d9ba050b4547363adc203f92336d49ab518036b
Sahil Amin <sahilamin219@gmail.com>
2020-06-07 22:08:00 +0530

    Add solution for problem `C - Rotation Matching` Link: https://codeforces.com/contest/1365/problem/C Tags: constructive algorithms, data structures, greedy, implementation, *1400 Ref: https://codeforces.com/contest/1365/submission/82866580

--- codeforces/1365/C.cpp
+++ codeforces/1365/C.cpp
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
 #define f(i,a,b) for(int i=a;i<b;i++)
 using namespace std;
+// vector<int> rotation(vector<int> v){
+// 	vector<<int> vec;
+// 	f(i,1,n)vec.push_back(v[i]);
+// 	vec.push_back(v[0]);
+// 	return vec;
+// }
 int main(){
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	int n;cin>>n;
 	vector<int> v(n);
 	vector<int> b(n);
 	f(i,0,n)cin>>v[i];
 	f(i,0,n)cin>>b[i];
+	// map<int,int> m;
 	vector<int> m(n,0);
 	f(i,0,n){
 		m[b[i]-1]=i;
