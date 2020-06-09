b93275d714441be18a66739b958db3023199b7c7
Sahil Amin <sahilamin219@gmail.com>
2020-06-10 00:23:00 +0530

    Add solution for problem `B - Uniqueness` Link: https://codeforces.com/contest/1208/problem/B Tags: binary search, brute force, implementation, two pointers, *1500 Ref: https://codeforces.com/contest/1208/submission/83248613

--- codeforces/1208/B.cpp
+++ codeforces/1208/B.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
+#define s(a) int(a.size())
 #define f(i,a,b) for(long long int i=a;i<b;i++)
 #define r(i,a,b) for(long long int i=a;i>b;i--) 
+#define pb push_back
+#define mkp make_pair
+#define mod 1000000007;
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define debug(x) cout<< #x << ':' << x << endl;
 using namespace std;
 typedef long long int ll;
+#define INF 0x3f3f3f3f;
 int main(){
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int n;cin>>n;
     vector<ll> v(n);
