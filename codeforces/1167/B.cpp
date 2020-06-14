898b2a84ee1ab9ff7f68f2a73b7d2dc5a0765001
Sahil Amin <sahilamin219@gmail.com>
2020-06-14 09:53:00 +0530

    Add solution for problem `B - Lost Numbers` Link: https://codeforces.com/contest/1167/problem/B Tags: brute force, divide and conquer, interactive, math, *1400 Ref: https://codeforces.com/contest/1167/submission/83713589

--- codeforces/1167/B.cpp
+++ codeforces/1167/B.cpp
@@ -1,8 +1,21 @@
+// i m n't bored yet ...
 #include <bits/stdc++.h>
+#define s(a) int(a.size())
 #define f(i,a,b) for(long long int i=a;i<b;i++)
+#define r(i,a,b) for(long long int i=a;i>b;i--) 
+#define pb push_back
+#define mkp make_pair
+#define mod 1000000007;
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define debug(x) cout<< #x << ':' << x << endl;
 using namespace std;
 typedef long long int ll;
+#define INF 0x3f3f3f3f;
 int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif//freopen("error.txt", "w", stderr);
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 	vector<int> p = {4, 8, 15, 16, 23, 42};
 	vector<ll> v(6);
