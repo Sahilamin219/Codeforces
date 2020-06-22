2112310b0d23957507f386e6b3072f6f6253a3ea
Sahil Amin <sahilamin219@gmail.com>
2020-06-22 12:08:00 +0530

    Add solution for problem `A - Takahashikun, The Strider` Link: https://atcoder.jp/contests/agc046/tasks/agc046_a Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/agc046/submissions/14607229

--- atcoder/agc046/A.py
+++ atcoder/agc046/A.py
@@ -1,7 +1,7 @@
 n=int(input())
+#n*k1=(360)*k2
+#(k1/k2)=(360/n)
 k=1
 while((k*n)%360!=0):
   k+=1
 print(k)
-#We can now regard Takahashi’s move as movingXdegrees on the circle,
-#so the answer is the minimumksuch thatkXis a multiple of 360
