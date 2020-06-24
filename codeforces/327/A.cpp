0e4bed9b6ee41fd317dd139d25c6f33498eda809
Sahil Amin <sahilamin219@gmail.com>
2020-06-24 18:48:00 +0530

    Add solution for problem `A - Flipping Game` Link: https://codeforces.com/contest/327/problem/A Tags: brute force, dp, implementation, *1200 Ref: https://codeforces.com/contest/327/submission/84891182

---
+++ codeforces/327/A.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+#define s(a) long (a.size())
+#define f(i,a,b) for(long long int i=a;i<b;i++)
+#define r(i,a,b) for(long long int i=a;i>b;i--) 
+#define fi first
+#define se second
+#define pb push_back
+#define mkp make_pair
+#define pi pair<int,int>
+#define mod 1000000007;
+#define bitcount    __builtin_popcountll
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define debug(x) cout<< #x << ':' << x << endl;
+#define show(x) cout<<x<<endl;
+#define pret(x) cout<<x<<endl; return 0;
+#define disp(x) cout<<x<<" ";
+#define let(x,y) cout<<x<<" "<<y<<endl;
+using namespace std;
+typedef long long int ll;
+#define INF 0x3f3f3f3f;
+long long maximum=numeric_limits<int>:: max();
+long long minimum=numeric_limits<int>:: min();
+string gg="abcdefghijklmnopqrstuvwxyz";
+string gv="aeiouy";//"AOYEUI";
+bool sortbysec(const pair<ll,ll> &a, const pair<ll,ll> &b)
+{
+    if(a.first==b.first){
+        return a.second<b.second;
+    }// return (a.second<b.second);
+    return (a.first>b.first);
+    return false;
+}
+string reverseStr(string& str) 
+{ 
+    int n = str.length(); 
+    for (int i = 0; i < n / 2; i++) 
+        swap(str[i], str[n - i - 1]); 
+    return str;
+} 
+vector<long long> trial_division1(long long n) {
+    vector<long long> factorization;
+    for (long long d = 2; d * d <= n; d++) {
+        while (n % d == 0) {
+            factorization.push_back(d);
+            n /= d;
+        }
+    }
+    if (n > 1)
+        factorization.push_back(n);
+    return factorization;
+}
+// ll n,m,k;
+// vector<string> str;
+// set<int> s;
+// int dp[101][100001];
+// int get(vector<int> v,int ind,int sum)
+// {
+// }
+vector<int> solve(int n){
+	vector<int> a(3,0);
+	int i=0;
+	while(n){
+		a[i]=(n%2);i++;
+		n/=2;
+	}
+	reverse(a.begin(), a.end());
+	// for(auto x: a)cout<<x<<" ";
+				// cout<<endl;
+	return a;
+}
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif//freopen("error.txt", "w", stderr);
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int n;cin>>n;
+	vector<int> v(n);
+	f(i,0,n)cin>>v[i];
+	int s=0;int e=n-1;
+	int ans=0;
+	while(s<n and v[s]==1){
+		ans++;
+		s++;
+	}
+	while(v[e]==1 and e>s){
+		e--;
+		ans++;
+	}
+	// cout<<s<<"--"<<e<<" ";
+	int one=0,zero=0,cnt=0;
+	f(i,s,e+1){
+		if(v[i]==0){
+			one++;
+		}
+		else{
+			zero++;
+		}
+		if(cnt<one-zero){
+			cnt=(one-zero);
+		}
+		if(one-zero<0){
+			zero=0;one=0;
+		}
+	}
+	if(*min_element(v.begin(), v.end())==1){
+		cout<<n-1;
+	}
+	else{
+		// cout<<zero<<"--"<<one<<"--"<<cnt<<" ";
+		// ans+=max(zero,one);
+		// if(zero>=one)ans+=cnt;
+		ans=0;
+		ans=count(v.begin(), v.end(), 1);
+		cout<<ans+cnt;
+	}
+	// i=(i-1)%v.size();
+	// v.erase(v.begin()+i, v.begin()+1+i);
+	// (si == 0) ? cout<<1<<endl : cout<<si<<"\n";
+	return 0;
+}
+// cout.flush();
+// fflush(stdout);
+// if(i!=l or (n+m)%2!=0)
+// puts(flag?"NO":"YES");
+// if(s-i>0 and find(v.begin(), v.end(), s-i)== v.end())
