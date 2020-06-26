75948c4cdce4bc3d90e995682d9ab90cfe7431e5
Sahil Amin <sahilamin219@gmail.com>
2020-06-26 20:59:00 +0530

    Add solution for problem `A - Three Friends` Link: https://codeforces.com/contest/1272/problem/A Tags: brute force, greedy, math, sortings, *900 Ref: https://codeforces.com/contest/1272/submission/85140395

---
+++ codeforces/1272/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define s(a) long (a.size())
+#define f(i,a,b) for(long long int i=a;i<b;i++)
+#define r(i,a,b) for(long long int i=a;i>b;i--) 
+#define fi first
+#define se second
+#define pb push_back
+#define mkp make_pair
+#define pi pair<int,int>
+#define mod 1000000007;
+#define lcm(a,b) ((a*b)/__gcd(a,b))
+#define zrobits(x)	__builtin_ctzll(x)
+#define bitcount    __builtin_popcountll
+#define ps(x,y)		fixed<<setprecision(y)<<x
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define debug(x) cout<< #x << ':' << x << endl;
+#define show(x) cout<<x<<endl;
+#define pret(x) cout<<x<<endl; return 0;
+#define disp(x) cout<<x<<" ";
+#define let(x,y) cout<<x<<" "<<y<<endl;
+using namespace std;
+typedef long long int ll;
+int main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t;cin>>t;
+	// int t=1;
+	while(t--){
+		int flag=1;
+		ll a,b,c;cin>>a>>b>>c;
+		ll x=abs(a-b)+abs(c-b)+abs(a-c)-4;
+		cout<<max(0LL,x)<<endl;
+	}
+	return 0;
+}
