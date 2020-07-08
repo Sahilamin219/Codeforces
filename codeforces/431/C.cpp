3c86c3d8da28e61071826529907cec1625f9274f
Sahil Amin <sahilamin219@gmail.com>
2020-07-08 19:19:00 +0530

    Add solution for problem `C - k-Tree` Link: https://codeforces.com/contest/431/problem/C Tags: dp, implementation, trees, *1600 Ref: https://codeforces.com/contest/431/submission/86279662

--- codeforces/431/C.cpp
+++ codeforces/431/C.cpp
@@ -1,67 +1,92 @@
-#include <bits/stdc++.h>
-#define s(a) long (a.size())
-#define f(i,a,b) for(long long int i=a;i<b;i++)
-#define r(i,a,b) for(long long int i=a;i>b;i--) 
-#define fi first
-#define se second
-#define pb push_back
-#define mkp make_pair
-#define pi pair<long long int,long long int>
-#define mapi map<long long int,long long int>
-#define mod 1000000007
-#define all(x) x.begin(), x.end()
-#define lcm(a,b) ((a*b)/__gcd(a,b))
-#define zrobits(x)	__builtin_ctzll(x)
-#define bitcount    __builtin_popcountll
-#define ps(x,y)		fixed<<setprecision(y)<<x
-#define negmod(X,Y) X % Y + (X %  Y < 0 ? Y : 0)
-#define flip_bits(number) static_cast<unsigned>(~number) 
-#define print() cout<<"\n";
-#define debug(x) cout<< #x << ':' << x << endl;
-#define show(x) cout<<x<<endl;
-#define pret(x) cout<<x<<endl; return 0;
-#define disp(x) cout<<x<<" ";
-#define let(x,y) cout<<x<<" "<<y<<endl;
+/* |     |           ___            |      |  __             __    __    __   __
+   |     |    /\    /   \ |    |    |      | |   \    /\    |   \ |   \ |    |  \
+   |-----|   /__\   \___  |____|    |  /\  | |__ /   /__\   |__ / |__ / |_   |__/
+   |     |  /    \      \ |    |    | /  \ | |  \   /    \  |     |     |    |  \
+   |     | /      \ \___/ |    |    |/    \| |   \ /      \ |     |     |__  |   \
+*/
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
-#define vi(v,n) vector<long long int> v(n)
-#define vvi(v,n,m) vector<vector<long long int> > v(n, vector<long long int> (m,0));
-#define INF 0x3f3f3f3f;
-long long maximum=numeric_limits<int>:: max();
-long long minimum=numeric_limits<int>:: min();
-void add(ll &a,  ll b)
-{
-	a+=b;
-	if(a>mod){a-=mod;}
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define lb lower_bound
+#define ub upper_bound
+#define pf push_front
+#define pb push_back
+#define ll long long
+#define pi pair<int,int>
+#define pl pair<long long,long long>
+#define pld pair<long double,long double>
+#define endl '\n'
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(int i=(a);i<=(b);i++)
+#define mod ((ll)(1e9+7))
+#define in(x) scanf("%lld",&x)
+#define in2(x,y) scanf("%lld %lld",&x,&y)
+#define in3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define inv(v) for(auto&i:v) in(i)
+#define all(x) x.begin(), x.end()
+#define vl vector<ll>
+#define ml unordered_map<ll,ll>
+#define mlc unordered_map<int,int,custom_hash> 
+#define vpl vector<pair<ll,ll>>
+#define INF 0x3f3f3f3f
+ 
+template<typename T, typename TT>
+ostream& operator<<(ostream &os, const pair<T, TT> &t) { return os<<t.first<<" "<<t.second; }
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T> &t) { for(auto& i: t) os<<i<<" "; return os; }
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+ll com(ll n,ll m){
+    ll ans=1;
+    rep(i,m+1,n){
+        ans*=i;
+    }rep(i,1,n-m){
+        ans/=i;
+    }
+    return ans;
 }
+
+
+ll n,k,d;ll pd[124][2];
+
+ll dp(ll m,ll c)
+{
+    if(m==0)return c;
+    if(m<=0)return 0;
+    if(pd[m][c]!=-1)return pd[m][c];
+    ll s=0;
+    if(pd[m][c]==-1)
+    {
+	    for(int i=1;i<=k;++i){
+	        s+=(dp(m-i,c||i>=d)%1000000007);
+	        s=(s%1000000007);
+	    }
+    }
+    pd[m][c]=s;
+    return s;
+}
+
 int main(){
 	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	//freopen("error.txt", "w", stderr);
-	#endif
-	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	ll n,k,d;cin>>n>>k>>d;
-	vector<vector<ll>> dp(102,vector<ll> (2,0));
-	dp[0][0]=1;
-	dp[0][1]=0;
-	f(i,1,n+1)
-	{
-		f(j,1,k+1)
-		{
-			if(i-j<0)break;
-			if(j<d)
-			{
-				add(dp[i][0],dp[i-j][0]);
-				add(dp[i][1],dp[i-j][1]);
-			}
-			else
-			{
-				add(dp[i][1],dp[i-j][0]);
-				add(dp[i][1],dp[i-j][1]);
-			}
-		}
-	}
-	cout<<dp[n][1];
-	return 0;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif//freopen("error.txt", "w", stderr);
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    in3(n,k,d);
+    memset(pd,-1,sizeof(pd));
+    cout<<(dp(n,0)%1000000007);
+
+    //cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    return 0;
 }
