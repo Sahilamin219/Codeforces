0b30c977b33102ff2926bdb16569f1c28c49fec2
Sahil Amin <sahilamin219@gmail.com>
2020-07-11 20:39:00 +0530

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/86521412

---
+++ codeforces/1372/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define s(a) long (a.size())
+#define f(i,a,b) for(long long int i=a;i<b;i++)
+#define r(i,a,b) for(long long int i=a;i>b;i--) 
+#define fi first
+#define se second
+#define pb push_back
+#define mkp make_pair
+#define pi pair<long long int,long long int>
+#define mapi map<long long int,long long int>
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+#define lcm(a,b) ((a*b)/__gcd(a,b))
+#define zrobits(x)	__builtin_ctzll(x)
+#define bitcount    __builtin_popcountll
+#define ps(x,y)		fixed<<setprecision(y)<<x
+#define negmod(X,Y) X % Y + (X %  Y < 0 ? Y : 0)
+#define flip_bits(number) static_cast<unsigned>(~number) 
+#define sp cout<<" "
+#define print() cout<<"\n";
+#define debug(x) cout<< #x << ':' << x << endl;
+#define show(x) cout<<x<<endl;
+#define pret(x) cout<<x<<endl; return 0;
+#define disp(x) cout<<x<<" ";
+#define let(x,y) cout<<x<<" "<<y<<endl;
+using namespace std;
+typedef long long int ll;
+#define resultv(v) accumulate(all(v),ll(0))
+#define productv(v) accumulate(all(v), ll(1), multiplies< ll >())
+#define difference(v) accumulate(all(v), ll(0), sum, std::minus<ll>()); 
+#define vi(v,n) vector<long long int> v(n)
+#define vvi(v,n,m) vector<vector<long long int> > v(n, vector<long long int> (m,0));
+#define INF 0x3f3f3f3f;
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	//freopen("error.txt", "w", stderr);
+	#endif
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	//don't know whats going on in eveyone's life
+	int t;cin>>t;
+	while(t--)
+	{
+		int n;cin>>n;
+		f(i,0,n)cout<<1<<" ";
+		cout<<endl;
+	}
+	return 0;
+}
