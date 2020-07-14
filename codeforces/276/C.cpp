b87a03c09d3692e3b30dffd025e9f6df81411ef4
Sahil Amin <sahilamin219@gmail.com>
2020-07-14 17:30:00 +0530

    Add solution for problem `C - Little Girl and Maximum Sum` Link: https://codeforces.com/contest/276/problem/C Tags: data structures, greedy, implementation, sortings, *1500 Ref: https://codeforces.com/contest/276/submission/86827738

--- codeforces/276/C.cpp
+++ codeforces/276/C.cpp
@@ -39,6 +39,11 @@ struct less_second {
         return a.second < b.second;
     }
 };
+#ifdef WIN32
+#define INT64 "%I64d"
+#else
+#define INT64 "%lld"
+#endif
 int getSum(vector<int> bit, int index)
 {
     ll sum=0;index++;
@@ -49,6 +54,11 @@ int getSum(vector<int> bit, int index)
 }
 int32_t main()
 {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    //freopen("error.txt", "w", stderr);
+    #endif
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     ll n,q;cin>>n>>q;
     vector<ll> v(n);f(i,0,n)cin>>v[i];
@@ -80,7 +90,58 @@ int32_t main()
         sum+=(v[i]*ind[i]);      
     }
     cout<<sum;
-    // NO NEED OF BIT....
-    // HAVE DONE SAME TYPE OF QUESTION BUT TOOK TOO LONG
     return 0;
 }
+// #include<bits/stdc++.h>
+// #define ll long long
+// #define PI 3.14159265
+// ll mod = 1000000009;
+// using namespace std;
+// template<typename T>
+// void debug(vector<T> x){
+//     for(auto e:x){
+//         cout<<e<<" ";// template<typename T>
+// void debug(vector<T> x){
+//     for(auto e:x){
+//         cout<<e<<" ";
+//     }cout<<"\n";
+// }
+//     }cout<<"\n";
+// }
+// ll dpHelper(vector<ll>& dp, ll n){
+//     if(dp[n]!=-1){
+//         return dp[n];
+//     }else{
+//         int x = dpHelper(dp,n-3);
+//         int y = dpHelper(dp, n-2);
+//         if(x==-1 && y== -1){
+//             dp[n] = -1;
+//         }else if(x == -1){
+//             dp[n] = y;
+//         }else if(y == -1){
+//             dp[n] = x;
+//         }else{
+//             dp[n] = ((x%mod +y%mod)%mod);
+//         }
+//         return dp[n];
+// }
+// int main(){
+//     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//     int t = 1;
+//     cin>>t;
+//     vector<ll> dp(1e6+5, -1);
+//     dp[0] = 1;
+//     dp[1] = 0;
+//     dp[2] = 1;
+//     dp[3] = 1;
+//     ll x,y;
+//     for(ll i=4; i<=1e6; i++){
+//         dp[i] = dpHelper(dp, i);
+//     }
+//     while(t--){
+//         ll n;
+//         cin>>n;
+//         cout<<dp[n]<<"\n";
+//     }
+//     return 0;
+// }
