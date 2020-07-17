1d9c957b5ec6069ce2379e2d246e9aa7b91818fd
Sahil Amin <sahilamin219@gmail.com>
2020-07-17 07:58:00 +0530

    Add solution for problem `A - Two Substrings` Link: https://codeforces.com/contest/550/problem/A Tags: brute force, dp, greedy, implementation, strings, *1500 Ref: https://codeforces.com/contest/550/submission/87033558

--- codeforces/550/A.cpp
+++ codeforces/550/A.cpp
@@ -60,5 +60,83 @@ int32_t main()
     d=s.find("AB", c+2);
     if((a!=-1 and b!=-1) or (c!=-1 and d!=-1))cout<<"YES";
     else{cout<<"NO";}
+    // ll m=1000000007;
+    // ll n;cin>>n;
+    // vector<vector<ll>> dp(2, vector<ll> (4,0));
+    // dp[0][0]=0;
+    // dp[0][1]=0;
+    // dp[0][2]=0;
+    // dp[0][3]=1;
+    // f(i,1,n+1)
+    // {
+    //     if(i%2==0){
+    //         dp[0][0]=(dp[1][3]+dp[1][2]+dp[1][1])%m;
+    //         dp[0][1]=(dp[1][2]+dp[1][3]+dp[1][0])%m;
+    //         dp[0][2]=(dp[1][1]+dp[1][0]+dp[1][3])%m;
+    //         dp[0][3]=(dp[1][2]+dp[1][0]+dp[1][1])%m;
+    //         // cout<<dp[0][3]<<" ";
+    //     }
+    //     else{
+    //         dp[1][0]=(dp[0][3]+dp[0][2]+dp[0][1])%m;
+    //         dp[1][1]=(dp[0][2]+dp[0][3]+dp[0][0])%m;
+    //         dp[1][2]=(dp[0][1]+dp[0][0]+dp[0][3])%m;
+    //         dp[1][3]=(dp[0][2]+dp[0][0]+dp[0][1])%m;
+    //         // cout<<dp[1][3]<<" ";
+    //     }
+    // }
+    // cout<<dp[n%2][3];
     return 0;
 }
+// #include<bits/stdc++.h>
+// #define ll long long
+// #define PI 3.14159265
+// ll mod = 1000000009;
+// using namespace std;
+// template<typename T>
+// void debug(vector<T> x){
+//     for(auto e:x){
+//         cout<<e<<" ";// template<typename T>
+// void debug(vector<T> x){
+//     for(auto e:x){
+//         cout<<e<<" ";
+//     }cout<<"\n";
+// }
+//     }cout<<"\n";
+// }
+// ll dpHelper(vector<ll>& dp, ll n){
+//     if(dp[n]!=-1){
+//         return dp[n];
+//     }else{
+//         int x = dpHelper(dp,n-3);
+//         int y = dpHelper(dp, n-2);
+//         if(x==-1 && y== -1){
+//             dp[n] = -1;
+//         }else if(x == -1){
+//             dp[n] = y;
+//         }else if(y == -1){
+//             dp[n] = x;
+//         }else{
+//             dp[n] = ((x%mod +y%mod)%mod);
+//         }
+//         return dp[n];
+// }
+// int main(){
+//     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//     int t = 1;
+//     cin>>t;
+//     vector<ll> dp(1e6+5, -1);
+//     dp[0] = 1;
+//     dp[1] = 0;
+//     dp[2] = 1;
+//     dp[3] = 1;
+//     ll x,y;
+//     for(ll i=4; i<=1e6; i++){
+//         dp[i] = dpHelper(dp, i);
+//     }
+//     while(t--){
+//         ll n;
+//         cin>>n;
+//         cout<<dp[n]<<"\n";
+//     }
+//     return 0;
+// }
