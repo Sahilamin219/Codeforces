f538b6e92812262eaded7494cca76d9a61aeae53
Sahil Amin <sahilamin219@gmail.com>
2020-08-08 14:11:00 +0530

    Add solution for problem `A - Rainbow Dash, Fluttershy and Chess Coloring` Link: https://codeforces.com/contest/1393/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1393/submission/89319794

---
+++ codeforces/1393/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dex(x,y) fixed<<setprecision(x)<<y
+int32_t main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        long long int n;
+        cin>>n;
+        cout<<n/2+1<<endl;
+    }
+    return 0;
+}
