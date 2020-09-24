f7973bd9149116d65ecf5a7c4529fd6746ae323a
Sahil Amin <sahilamin219@gmail.com>
2020-09-25 01:03:00 +0530

    Add solution for problem `A - Cubes Sorting` Link: https://codeforces.com/contest/1420/problem/A Tags: math, sortings, *900 Ref: https://codeforces.com/contest/1420/submission/93729829

---
+++ codeforces/1420/A.cpp
@@ -0,0 +1,126 @@
+// "I don't need anyone. I just need everyone and then some"
+#include <bits/stdc++.h>
+#define sz(a) long (a.size())
+#define f(i,a,b) for(long long int i=a;i<b;i++)
+#define r(i,a,b) for(long long int i=a;i>b;i--)
+#define pb push_back
+#define mkp make_pair
+#define mod 1000000009
+#define MAX 100000
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define debug(x) cerr<< #x << ':' << x << endl;
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+#define fit(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++ )
+using namespace std;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) 
+{return (a.second < b.second);}
+
+int minSwaps(int arr[], int n) 
+{ 
+    // Create an array of pairs where first 
+    // element is array element and second element 
+    // is position of first element 
+    pair<int, int> arrPos[n]; 
+    for (int i = 0; i < n; i++) 
+    { 
+        arrPos[i].first = arr[i]; 
+        arrPos[i].second = i; 
+    } 
+  
+    // Sort the array by array element values to 
+    // get right position of every element as second 
+    // element of pair. 
+    sort(arrPos, arrPos + n); 
+  
+    // To keep track of visited elements. Initialize 
+    // all elements as not visited or false. 
+    vector<bool> vis(n, false); 
+  
+    // Initialize result 
+    int ans = 0; 
+  
+    // Traverse array elements 
+    for (int i = 0; i < n; i++) 
+    { 
+        // already swapped and corrected or 
+        // already present at correct pos 
+        if (vis[i] || arrPos[i].second == i) 
+            continue; 
+  
+        // find out the number of  node in 
+        // this cycle and add in ans 
+        int cycle_size = 0; 
+        int j = i; 
+        while (!vis[j]) 
+        { 
+            vis[j] = 1; 
+  
+            // move to next node 
+            j = arrPos[j].second; 
+            cycle_size++; 
+        } 
+  
+        // Update answer by adding current cycle.  
+        if (cycle_size > 0) 
+        { 
+            ans += (cycle_size - 1); 
+        } 
+    } 
+  
+    // Return result 
+    return ans; 
+} 
+  
+
+int main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    // brute force greedy math 
+    // __builtin_popcount
+    int t;cin>>t;
+    while(t--)
+    {
+        ll n;cin>>n;
+        vi a(n), v(n);
+        f(i,0,n){cin>>a[i];v[i]=a[i];}
+        sort(rall(v));
+        set<ll> s;
+        f(i,0,n)s.insert(v[i]);
+        if(a==v and v[0]!=v[n-1] and s.size()==n)cout<<"NO\n";
+        else cout<<"YES\n";
+    }
+
+}
+// ifstream  i_data("data.in");
+// ofstream  o_data("data.out");
+// #define cin  i_data
+// #define cout o_data
+//ifstream cin("b_read_on.txt"); ofstream cout("output3.txt");
+//Use (<<) for multiplication
+//Use (>>) for division
+//ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout<<fixed;cerr.tie(NULL);
+// find_by_order -> value at index
+// order_of_key -> index of value
+// while using (1<<i) use ((ll)1<<(ll)i) 
+// in Floyd-Warshall Algo, k is outer loop 
+// If an element was not initially in map and if asked mp[a],the element gets inserted 
+// a%=mod take a lot of time... try to use it minimum and use memset as it reduces a lot of time usage...use if(a>=mod) a%=mod
+//cout<<(double) can be harmful , always use printf(%.9llf)...take scanf("%lf",&p[i][j]) as input , not llf;
+//use s.erase(it++) for erasing iterator and then moving to the next one
+//never use adj.resize(n) as value is persistent, always erase
+//use __builtin_popcountll() for ll
+// no of prime numbers in range : (70,19) , (1000,168) , (100000,1229) , (sqrt(10^9),3409) ;
+//always check the use of segment tree using bottom-    up dp
+//__gcd(0,0) gives runtime error
+//power(a,b) == power(a,b%phi(a)) Fermet's Theorem
+//never use "=" operator in compare function
+//For checking odd cycles, check bipartite
