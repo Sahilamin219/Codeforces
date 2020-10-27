806dbdc160aaa27d038c49e6bc7cba6533cc3d70
Sahil Amin <sahilamin219@gmail.com>
2020-10-27 10:30:00 +0530

    Add solution for problem `B - The Festive Evening` Link: https://codeforces.com/contest/834/problem/B Tags: data structures, implementation, *1100 Ref: https://codeforces.com/contest/834/submission/96825920

---
+++ codeforces/834/B.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define sz(a) long (a.size())
+#define f(i,a,b) for(long long int i=a;i<b;i++)
+#define fr(i,a,b) for(long long int i=a;i>b;i--)
+#define pb push_back
+#define mkp make_pair
+#define mod 1000000009
+#define MAX 100000
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define debug(x) cerr<< #x << ':' << x << endl;
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+#define fit(it,c) for ( __typeof((c).begin()) it=(c).begin(); it!=(c).end(); it++ )
+using namespace std;
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) 
+{return (a.second < b.second);}
+
+
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int n,k;cin>>n>>k;
+    string s;cin>>s;
+    unordered_map<char, int> last_pos;
+    unordered_set<char> open_doors;
+    f(i,0,n)
+    {
+        last_pos[s[i]]=i;
+    }
+    f(i,0,n)
+    {
+        open_doors.insert(s[i]);
+        if(open_doors.size()==k+1){cout<<"yes";goto skip;}
+        if(i==last_pos[s[i]])open_doors.erase(s[i]);
+    }
+    cout<<"no"<<endl;
+    skip:;
+    return 0;
+}
+// To heapify a subtree rooted with node i which is 
+// an index in arr[]. n is size of heap 
+void heapify(int arr[], int n, int i) 
+{ 
+    int largest = i; // Initialize largest as root 
+    int l = 2*i + 1; // left = 2*i + 1 
+    int r = 2*i + 2; // right = 2*i + 2 
+  
+    // If left child is larger than root 
+    if (l < n && arr[l] > arr[largest]) 
+        largest = l; 
+  
+    // If right child is larger than largest so far 
+    if (r < n && arr[r] > arr[largest]) 
+        largest = r; 
+  
+    // If largest is not root 
+    if (largest != i) 
+    { 
+        swap(arr[i], arr[largest]); 
+  
+        // Recursively heapify the affected sub-tree 
+        heapify(arr, n, largest); 
+    } 
+} 
+  
+// main function to do heap sort 
+void heapSort(int arr[], int n) 
+{ 
+    // Build heap (rearrange array) 
+    for (int i = n / 2 - 1; i >= 0; i--) 
+        heapify(arr, n, i); 
+  
+    // One by one extract an element from heap 
+    for (int i=n-1; i>0; i--) 
+    { 
+        // Move current root to end 
+        swap(arr[0], arr[i]); 
+  
+        // call max heapify on the reduced heap 
+        heapify(arr, i, 0); 
+    } 
+} 
