60b1bd83faf4aa33111afb5712ad0cf7ac57e356
Sahil Amin <sahilamin219@gmail.com>
2020-11-29 18:07:00 +0530

    Add solution for problem `C - Engineer Artem` Link: https://codeforces.com/contest/1438/problem/C Tags: 2-sat, chinese remainder theorem, constructive algorithms, fft, flows, *2000 Ref: https://codeforces.com/contest/1438/submission/99903167

---
+++ codeforces/1438/C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define F first
+#define S second
+#define mod 1000000009
+#define MAX 100001
+#define all(x) x.begin(), x.end()
+#define negmod(X,Y) X % Y + (X % Y < 0 ? Y : 0)
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vi;
+typedef vector<vi> vvi;
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {return (a.second < b.second);}
+static bool mysort(pair<int, int>& a, pair<int, int>& b){ return a.second==b.second?a.first<b.first:a.second<b.second;}
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+struct cmp {
+	bool operator() (pair<int, int> a, pair<int, int> b) {
+		 return a.first+a.second > b.first+b.second;
+	}
+};
+void solve()
+{
+	ll n,m;cin>>n>>m;
+	vvi v(n, vi (m));
+	ll i,j;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)cin>>v[i][j];
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			cout << v[i][j] + !((v[i][j]&1) == ((i+j)&1))<<" ";
+		}cout<<endl;
+	}
+}
+int main(int argc, char const *argv[]){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+	#endif
+	int t=1;cin>>t;
+	while(t--)solve();
+	return 0;
+}
