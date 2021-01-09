6e56e448c752e204d66de7604dcd8d4b526029c6
Sahil Amin <sahilamin219@gmail.com>
2021-01-09 13:13:00 +0530

    Add solution for problem `D - 13th Labour of Heracles` Link: https://codeforces.com/contest/1466/problem/D Tags: data structures, greedy, sortings, trees, *1500 Ref: https://codeforces.com/contest/1466/submission/103857100

--- codeforces/1466/D.cpp
+++ codeforces/1466/D.cpp
@@ -61,9 +61,29 @@ void solve_me()
 	for(auto &i:to_Sort){cout<<ans<<" "; ans += i;}
 		cout<<ans<<"\n";
 }
-int main(){
+int main(int argc, char const *argv[]){
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+	int tt = clock();
+	#endif
+	// cout << fixed << setprecision(9);
 	int Testcase=1;assert(cin>>Testcase);
 	while(Testcase--)solve_me();
+	#ifndef ONLINE_JUDGE
+		cerr << "TIME = " << clock() - tt << endl;
+		tt = clock();
+	#endif
 	return 0;
 }
+
+// Deletes all characters between 0th index and str.end() - 6
+    //str.erase(str.begin() + 0, str.end() - 6);
+// Deletes character at position 4
+    //str.erase(str.begin() + 4);
+// Deletes 4 characters from index number 1
+	//str.erase(1, 4);
+// Deletes all characters except first one
+   //str.erase(1);
+// Deletes all characters
+   //str.erase(); 
