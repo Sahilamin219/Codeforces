2d2bab589b802fda33a2d8f34eba1ba0b1fe5d62
Sahil Amin <sahilamin219@gmail.com>
2021-01-24 03:00:00 +0530

    Add solution for problem `B - Last Year's Substring` Link: https://codeforces.com/contest/1462/problem/B Tags: dp, implementation, strings, *800 Ref: https://codeforces.com/contest/1462/submission/105183692

---
+++ codeforces/1462/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+typedef vector<int> vi;
+struct hash_pair {
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return (hash1 ^ hash2);
+    }
+};
+const int maxn = (2e5 + 5);
+const string T="2020";
+void solve_me()
+{
+	int n;cin>>n;
+	string s;cin>>s;
+	for(int i=0;i<=4;i++)
+	{
+		if (s.substr(0, i) + s.substr(n - 4 + i, 4 - i) == T) 
+		{
+			cout<<"YES\n";return;
+		}
+	}
+	cout<<"NO\n";
+	return;
+}
+
+int32_t main( )
+{
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+	int tt = clock();
+	#endif
+	// cout << fixed << setprecision(18);
+	int Testcase=1;assert(cin>>Testcase);
+	while(Testcase--)solve_me();
+	#ifndef ONLINE_JUDGE
+		cerr << "TIME = " << clock() - tt << endl;
+		tt = clock();
+	#endif
+	return 0;
+}
