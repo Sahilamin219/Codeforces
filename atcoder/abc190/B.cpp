fd63609297b937558d51902b9f5ce335254f3f50
Sahil Amin <sahilamin219@gmail.com>
2021-01-30 17:51:00 +0530

    Add solution for problem `B - Magic 3` Link: https://atcoder.jp/contests/abc190/tasks/abc190_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc190/submissions/19793871

---
+++ atcoder/abc190/B.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <string_view>
+#define int long long
+#define all(x) begin(x), end(x)
+using namespace std;
+typedef vector<int> vi;
+// T(n) = sum_i=1^n(T(i-1) * T(n-i))
+int X[4]={1,0,-1,0};
+int Y[4]={0,1,0,-1};
+const int cons=501;
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {return (a.second < b.second);}
+static bool mysort(pair<int, int>& a, pair<int, int>& b){ return a.second==b.second?a.first<b.first:a.second<b.second;}
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+struct cmp {
+  bool operator() (pair<int, int> a, pair<int, int> b) {
+     return a.first+a.second > b.first+b.second;
+  }
+};
+const int maxn = 2e5;
+long long ans;
+vector<long long> subtree;
+vector<vector<int>> graph;
+long long computeSubtree(int node, int parent) {
+  subtree[node] = 1;
+  for(auto i : graph[node]) {
+    if(i == parent) continue;
+    subtree[node] += computeSubtree(i, node);
+  }
+  return subtree[node];
+}
+#define mod 1000000009
+int takemod(int a){return ((a%mod)+mod)%mod;}
+ 
+int fast_exp(int base, int expo) {
+  int res=1;
+  while(expo>0) {
+    if(expo&1) res=(res*base)%mod;
+    base=(base*base)%mod;
+    expo>>=1;}
+  return res;}
+ 
+int modinv(int a){return takemod(fast_exp(takemod(a), mod-2));}
+void solve()
+{
+  int n,s,d;cin>>n>>s>>d;
+  int flag=0;
+  for(int i=0;i<n;i++)
+  {
+    int a,b;cin>>a>>b;
+    if ( s > a and d < b)
+    {
+      flag=1;
+    }
+  }
+  if(flag)
+  {
+    cout<<"Yes\n";
+  }
+  else
+  {
+    cout<<"No\n";
+  }
+  return;
+}
+int32_t main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  int TC=1;//cin>>TC;
+  while(TC--)solve();
+}
