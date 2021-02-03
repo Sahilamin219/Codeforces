6705e79216da1cf647eedcfb6cf014d9f98077c8
Sahil Amin <sahilamin219@gmail.com>
2021-02-03 17:30:00 +0530

    Add solution for problem `D - Journey` Link: https://codeforces.com/contest/1476/problem/D Tags: dfs and similar, dp, dsu, implementation, *1700 Ref: https://codeforces.com/contest/1476/submission/106365166

---
+++ codeforces/1476/D.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+#include <string_view>
+#define int long long
+#define all(x) begin(x), end(x)
+using namespace std;
+typedef vector<int> vi;
+// T(n) = sum_i=1^n(T(i-1) * T(n-i))
+int X[4]={1,0,-1,0};
+int Y[4]={0,1,0,-1};
+const int cons=501;
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {return (a.second < b.second);}
+static bool mysort(pair<int, int>& a, pair<int, int>& b){ return a.second==b.second?a.first<b.first:a.second<b.second;}
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+struct cmp {
+  bool operator() (pair<int, int> a, pair<int, int> b) {
+     return a.first+a.second > b.first+b.second;
+  }
+};
+const int maxn = 2e5;
+long long ans;
+vector<long long> subtree;
+vector<vector<int>> graph;
+long long computeSubtree(int node, int parent) {
+  subtree[node] = 1;
+  for(auto i : graph[node]) {
+    if(i == parent) continue;
+    subtree[node] += computeSubtree(i, node);
+  }
+  return subtree[node];
+}
+#define mod 1000000009
+int takemod(int a){return ((a%mod)+mod)%mod;}
+ 
+int fast_exp(int base, int expo) {
+  int res=1;
+  while(expo>0) {
+    if(expo&1) res=(res*base)%mod;
+    base=(base*base)%mod;
+    expo>>=1;}
+  return res;}
+ 
+int modinv(int a){return takemod(fast_exp(takemod(a), mod-2));}
+int mint(int x, int y, int z) 
+{   
+    if (x < y) return (x < z)? x : z; 
+    else return (y < z)? y : z; 
+}
+void solve_me()
+{
+  int n, m, i;cin>>n;
+  string s;cin>>s;
+  vector<int> ans(n+1,1);
+  int cities_accumulated = 0;
+  int last_city_visited = -1;
+  for(i=0;i<n;i++)
+  {
+    if( last_city_visited != 1 and s[i]=='L')
+    {
+      cities_accumulated++;
+    }
+    else if(last_city_visited !=0 and s[i]=='R')
+    {
+      cities_accumulated++;
+    }
+    else
+    {
+      cities_accumulated=1;
+    }
+    last_city_visited=(s[i] == 'L');
+    if(s[i] == 'L')
+    {
+      ans[i+1] += cities_accumulated;
+    }
+  }
+  last_city_visited=-1;
+  cities_accumulated=0;
+  for(i=n-1;i>=0;i--)
+  {
+    if( last_city_visited != 1 and s[i]=='L')
+    {
+      cities_accumulated++;
+    }
+    else if(last_city_visited !=0 and s[i]=='R')
+    {
+      cities_accumulated++;
+    }
+    else
+    {
+      cities_accumulated=1;
+    }
+    last_city_visited=(s[i] == 'L');
+    if(s[i] == 'R')
+    {
+      ans[i] += cities_accumulated;
+    } 
+  }
+
+  for(int i=0;i<=n;i++)
+  {
+    cout<<ans[i]<< " ";
+  }cout<<"\n";
+  return;
+}
+int32_t main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+	#endif
+  // cout << fixed << setprecision(18);
+  int TC=1;cin>>TC;
+  while(TC--)solve_me();
+}
+// https://app.pluralsight.com/course-player?clipId=db4472d5-8c3b-416a-857b-f483e8fd9d82
+// Approach 1: Linear Scan
+// http://www.ijesrt.com/issues%20pdf%20file/Archives-2014/March-2014/86.pdf
+// https://www.eetimes.com/power-dissipation-in-portables-design-considerations-using-low-power-cmos-ics/
+
+void mydp()
+{
+    vector<vector<int>> dp(2, vector<int>(101,mod));
+    dp[1][0]=0;
+    vector<int> v(101);
+    for(int i=0;i<101;i++)
+    {
+        dp[0][i] = dp[1][i - 1] + v[i - 1];
+        dp[1][i] = dp[0][i - 1];
+        if(i>1) {
+            dp[0][i] = min(dp[0][i], dp[1][i - 2] + v[i - 1] + v[i - 2]);
+            dp[1][i] = min(dp[1][i], dp[0][i - 2]);
+        }
+    }
+    cout<<min(dp[0][101], dp[1][101])<<"\n";
+}
