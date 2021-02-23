5f017c42a16ab305a2bf4ae87cb5cd0a3539bb79
Sahil Amin <sahilamin219@gmail.com>
2021-02-23 15:54:00 +0530

    Add solution for problem `A - Three swimmers` Link: https://codeforces.com/contest/1492/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1492/submission/108278268

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-6 | [B - Vitamins](https://codeforces.com/contest/1042/problem/B) | [GNU C++17](./codeforces/1042/B.cpp) | `bitmasks` `brute force` `dp` `implementation` `*1200` | Mar/03/2021 20:51 | 
-5 | [J - Spelling Check](https://codeforces.com/contest/39/problem/J) | [GNU C++17](./codeforces/39/J.cpp) | `hashing` `implementation` `strings` `*1500` | Mar/03/2021 16:07 | 
-4 | [B - Berland Crossword](https://codeforces.com/contest/1494/problem/B) | [GNU C++17](./codeforces/1494/B.cpp) | `bitmasks` `brute force` `greedy` `implementation` `*1400` | Mar/02/2021 21:38 | 
-3 | [A - ABC String](https://codeforces.com/contest/1494/problem/A) | [GNU C++17](./codeforces/1494/A.cpp) | `bitmasks` `brute force` `implementation` `*900` | Mar/02/2021 20:29 | 
-2 | [D - Common Divisors](https://codeforces.com/contest/182/problem/D) | [GNU C++17](./codeforces/182/D.cpp) | `brute force` `hashing` `implementation` `math` `strings` `*1400` | Feb/28/2021 13:36 | 
-1 | [C - Maximum width](https://codeforces.com/contest/1492/problem/C) | [GNU C++17](./codeforces/1492/C.cpp) | `binary search` `data structures` `dp` `greedy` `two pointers` `*1500` | Feb/25/2021 21:25 | 
+7 | [B - Vitamins](https://codeforces.com/contest/1042/problem/B) | [GNU C++17](./codeforces/1042/B.cpp) | `bitmasks` `brute force` `dp` `implementation` `*1200` | Mar/03/2021 20:51 | 
+6 | [J - Spelling Check](https://codeforces.com/contest/39/problem/J) | [GNU C++17](./codeforces/39/J.cpp) | `hashing` `implementation` `strings` `*1500` | Mar/03/2021 16:07 | 
+5 | [B - Berland Crossword](https://codeforces.com/contest/1494/problem/B) | [GNU C++17](./codeforces/1494/B.cpp) | `bitmasks` `brute force` `greedy` `implementation` `*1400` | Mar/02/2021 21:38 | 
+4 | [A - ABC String](https://codeforces.com/contest/1494/problem/A) | [GNU C++17](./codeforces/1494/A.cpp) | `bitmasks` `brute force` `implementation` `*900` | Mar/02/2021 20:29 | 
+3 | [D - Common Divisors](https://codeforces.com/contest/182/problem/D) | [GNU C++17](./codeforces/182/D.cpp) | `brute force` `hashing` `implementation` `math` `strings` `*1400` | Feb/28/2021 13:36 | 
+2 | [C - Maximum width](https://codeforces.com/contest/1492/problem/C) | [GNU C++17](./codeforces/1492/C.cpp) | `binary search` `data structures` `dp` `greedy` `two pointers` `*1500` | Feb/25/2021 21:25 | 
+1 | [A - Three swimmers](https://codeforces.com/contest/1492/problem/A) | [GNU C++17](./codeforces/1492/A.cpp) | `math` `*800` | Feb/23/2021 15:54 | 
---
+++ codeforces/1492/A.cpp
@@ -0,0 +1,96 @@
+#include "bits/stdc++.h"
+#include <string_view>
+#define int long long
+#define all(x) begin(x), end(x)
+using namespace std;
+typedef vector<int> vi;
+// T(n) = sum_i=1^n(T(i-1) * T(n-i))
+int X[4]={1,0,-1,0};
+int Y[4]={0,1,0,-1};
+const int cons=501;
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {return (a.second < b.second);}
+static bool mysort(pair<int, int>& a, pair<int, int>& b)
+{
+  return a.first == b.first ? a.second < b.second : a.first > b.first;
+}
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+struct cmp {
+  bool operator() (pair<int, int> a, pair<int, int> b) {
+     return a.first+a.second > b.first+b.second;
+  }
+};
+
+#define mod 1000000009
+int takemod(int a){return ((a%mod)+mod)%mod;}
+int fast_exp(int base, int expo) {
+  int res=1;
+  while(expo>0) {
+    if(expo&1) res=(res*base)%mod;
+    base=(base*base)%mod;
+    expo>>=1;}
+  return res;}
+ 
+int modinv(int a){return takemod(fast_exp(takemod(a), mod-2));}
+int mint(int x, int y, int z) 
+{   
+    if (x < y) return (x < z)? x : z; 
+    else return (y < z)? y : z; 
+}
+void solve_me()
+{
+  int p,a,b,c;cin>>p>>a>>b>>c;
+  // int m=(a*b)/__gcd(a, b);
+  // m=(m*c)/__gcd(m, c);
+  // int d=m;
+  // while(m<p)
+  // {
+  //   m*=d;
+  // }
+  // cout<<m-p<<"\n";
+  int A= p < a ? a-p : ceil(1.0*p/a)*a - p;
+  int B= p < b ? b-p : ceil(1.0*p/b)*b - p;
+  int C= p < c ? c-p : ceil(1.0*p/c)*c - p;
+  cout << min( A, min(B, C) ) <<"\n";
+  return;
+}
+int32_t main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+	#endif
+  // cout << fixed << setprecision(18);
+  int TC=1;cin>>TC;
+  while(TC--)solve_me();
+}
+// https://app.pluralsight.com/course-player?clipId=db4472d5-8c3b-416a-857b-f483e8fd9d82
+// Approach 1: Linear Scan //         // cyclic sort - place all numbers to match their indexes 
+// http://www.ijesrt.com/issues%20pdf%20file/Archives-2014/March-2014/86.pdf
+// https://www.eetimes.com/power-dissipation-in-portables-design-considerations-using-low-power-cmos-ics/
+
+// https://recommender.codedrills.io/recommendations/a84cb4e1-1d54-4e0f-8f64-de726b77d05b
+// https://recommender.codedrills.io/recommendations/ebfb9d6e-fbd3-434e-89fb-8070b449c9ed
+int minSwapToMakeArraySame(int a[], int b[], int n)
+{
+    // map to store position of elements in array B
+    // we basically store element to index mapping.
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+        mp[b[i]] = i;
+ 
+    // now we're storing position of array A elements
+    // in array B.
+    for (int i = 0; i < n; i++)
+        b[i] = mp[a[i]];
+ 
+    /* We can uncomment this section to print modified
+      b array
+    for (int i = 0; i < N; i++)
+        cout << b[i] << " ";
+    cout << endl; */
+ 
+    // returing minimum swap for sorting in modified
+    // array B as final answer
+    return 1;//minSwapsToSort(b, n);
+}
