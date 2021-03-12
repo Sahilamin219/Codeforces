2513e5128db92fd26bb821b77d124917f78e9945
Sahil Amin <sahilamin219@gmail.com>
2021-03-12 18:56:00 +0530

    Add solution for problem `A - Anti-knapsack` Link: https://codeforces.com/contest/1493/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1493/submission/109786539

---
+++ codeforces/1493/A.cpp
@@ -0,0 +1,67 @@
+#include "bits/stdc++.h"
+#include <string_view>
+#define int long long
+#define all(x) begin(x), end(x)
+using namespace std;
+typedef vector<int> vi;
+int X[4]={1,0,-1,0};
+int Y[4]={0,1,0,-1};
+const int cons=501;
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b) {return (a.second < b.second);}
+static bool mysort(pair<int, int>& a, pair<int, int>& b)
+{
+  return a.first == b.first ? a.second < b.second : a.first > b.first;
+}
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+struct cmp {
+  bool operator() (pair<int, int> a, pair<int, int> b) {
+     return a.first+a.second > b.first+b.second;
+  }
+};
+
+#define mod 1000000009
+
+
+static bool UseMe(pair<int, int> &a, pair<int, int> &b){
+  return a.second > b.second;
+}
+
+struct hash_pair {
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const{
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return (hash1 ^ hash2);
+    }
+};
+
+void solve_me(){
+  int n,k;cin>>n>>k;
+  vi v(n+1, 0);
+  vi res;
+  for(int i=k-1;i;i--){
+    if(v[i]==0){
+      res.push_back(i);
+      v[k-i]=1;
+    }
+    else break;
+  }
+  for(int i=k+1;i<=n;i++){
+    res.push_back(i);
+  }
+  cout<<res.size()<<"\n";
+  for(auto x:res){cout<<x<<" ";}cout<<"\n";
+  return;
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+	#endif
+  cout << fixed << setprecision(18);
+  int TC=1;cin>>TC;
+  while(TC--)solve_me();
+}
