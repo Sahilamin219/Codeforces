74f72c92d087f22cd83eba92af6e8268479f1bb3
Sahil Amin <sahilamin219@gmail.com>
2021-03-19 14:22:00 +0530

    Add solution for problem `B - Binary Removals` Link: https://codeforces.com/contest/1499/problem/B Tags: brute force, dp, greedy, implementation Ref: https://codeforces.com/contest/1499/submission/110432098

--- codeforces/1499/B.cpp
+++ codeforces/1499/B.cpp
@@ -3,7 +3,31 @@
 #define all(x) begin(x), end(x)
 using namespace std;
 typedef vector<int> vi;
-
+const int maxn = 300002;
+vector<int> gr[maxn];
+string s;
+int n, m,  ans=1;
+vector<int> vis(maxn, 0), ancestors(maxn, 0);
+vector<vector<int>> dp(maxn, vector<int>(26, 0));
+void dfs(int node){
+  vis[node]=1;
+  ancestors[node]=1;
+  for(auto child: gr[node]){
+    if(ancestors[child]){
+      puts("-1");
+      exit(0);
+    }
+    if(!vis[child]){
+      dfs(child);
+    }
+    for(int i=0;i<26;i++){
+        dp[node][i]=max(dp[node][i], dp[child][i]);
+      }
+  }
+  dp[node][s[node]-'a']++;
+  for(int i=0;i<26;i++)ans=max(ans, dp[node][i]);
+  ancestors[node]=0;
+}
 void solve(){
   string s, a;cin>>s;a=s;
   sort(all(a));
