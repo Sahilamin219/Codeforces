0bda53f71a0f1d4a58fc5cd48cc5c20c574f38c6
Sahil Amin <sahilamin219@gmail.com>
2021-04-07 18:11:00 +0530

    Add solution for problem `B - Count Subrectangles` Link: https://codeforces.com/contest/1323/problem/B Tags: binary search, greedy, implementation, *1500 Ref: https://codeforces.com/contest/1323/submission/112224634

--- codeforces/1323/B.cpp
+++ codeforces/1323/B.cpp
@@ -1,53 +1,73 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
+
 using namespace std;
 typedef long long ll;
-int main()
-{
-	int n,m,k;
-	cin>>n>>m>>k;
-	int a[n],b[m];
-	for(int i=0;i<n;i++)cin>>a[i];
-	for(int i=0;i<m;i++)cin>>b[i];
-	
-	set<int> fac;
-	
-	for(int  i=1;i<=sqrt(k);i++)
-	{
-		if((k%i)==0){fac.insert(i);fac.insert(k/i);}
-	}	
-	ll ans=0;
-	int prea[n+1];memset(prea, 0, sizeof prea);
-	int preb[m+1];memset(preb, 0, sizeof preb);
-	for(int i=1;i<=n;i++)prea[i]=a[i-1]+prea[i-1];
-	for(int i=1;i<=m;i++)preb[i]=b[i-1]+preb[i-1];
-	for(auto x:fac)
-	{
-		int length=x, breadth=k/x;
-		//cout<<"l "<<length<<" "<<breadth<<"\n";
-		ll la=0, bb=0;int j;
-		for(int i=0;i<=n-length;i++)
-		{
-			j=i+length;
-			ll ct=prea[j]-prea[i];
-			if(ct==length)la++;
-			
-			
-		}
-		for(int i=0;i<=m-breadth;i++)
-		{
-			j=i+breadth;
-			ll ct=preb[j]-preb[i];
-			if(ct==breadth)bb++;
-			
-			
-		}
-		ans=ans+bb*la;
-		
-		
-		
-		
+
+int main() {
+	ll n,m,k;
+	cin >> n >> m >> k;
+	vector<ll> a(n);
+	vector<ll> b(m);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
 	}
-	cout<<ans;
-	
-	
+	for (int i = 0; i < m; ++i) {
+		cin >> b[i]; 
+	}
+	vector<ll> v;
+	for (ll i = 1; i*i <= k; ++i) {
+		if(k%i == 0) {
+			v.push_back(i);
+			if(i*i == k)continue;
+			v.push_back(k/i);
+		}
+	}
+	//v.push_back(k);
+	ll ans = 0;
+	for (int i = 0; i < (int)v.size(); ++i) {
+		ll cnt = 0;
+		ll x = 0;
+		ll y = 0;
+		for (int j = 0; j < n; ++j) {
+			if (a[j] == 1) {
+				cnt++;
+			}
+			if (a[j] == 0) {
+				cnt = 0;
+			}
+			if (cnt >= v[i]) {
+				x++;
+			} 
+		}
+		cnt = 0;
+		for (int j = 0; j < m; ++j) {
+			if (b[j] == 1) {
+				cnt++;
+			}
+			if (b[j] == 0) {
+				cnt = 0;
+			}
+			if (cnt >= k/v[i]) {
+				y++;
+			} 
+		}
+		ans += x*y;
+	}
+	cout << ans;
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
