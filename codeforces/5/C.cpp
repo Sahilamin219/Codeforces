67f5a24bc785f215c1815e1e5920889b7116d75a
Sahil Amin <sahilamin219@gmail.com>
2021-04-17 15:37:00 +0530

    Add solution for problem `C - Longest Regular Bracket Sequence` Link: https://codeforces.com/contest/5/problem/C Tags: constructive algorithms, data structures, dp, greedy, sortings, strings, *1900 Ref: https://codeforces.com/contest/5/submission/113318450

--- codeforces/5/C.cpp
+++ codeforces/5/C.cpp
@@ -1,53 +1,63 @@
-#include<bits/stdc++.h>
-#define REP(i,n) for (int i = 1; i <= n; i++)
-#define mod 1000000007
-#define pb push_back
-#define ff first
-#define ss second
-#define ii pair<int,int>
-#define vi vector<int>
-#define vii vector<ii>
-#define lli long long int
-#define INF 1000000000
-#define endl '\n'
-const double PI = 3.141592653589793238460;
-typedef std::complex<double> Complex;
-typedef std::valarray<Complex> CArray;
-
+#include "bits/stdc++.h"
+#define int long long
+#define all(x) begin(x), end(x)
 using namespace std;
-int dp[1000001] , C[1000001] , D[1000001];
-stack<int> stk;
-
-int main()
-{
-	string st;
-	int mx = 0;
-	
-	cin>>st;
-	for(int i=0;i<st.size();i++){
-		char ch = st[i];
-		
-		if(ch == '('){
-			stk.push(i);
-		}
-		else{
-			if(stk.empty()){
-				C[i] = D[i] = -1;
-			}
-			else{
-				int pos = stk.top();
-				stk.pop();
-				
-				C[i] = D[i] = pos;
-				if(pos > 0 && st[pos-1] == ')' && C[pos-1] != -1)
-				C[i] = C[pos-1];
-				
-				dp[i - C[i] + 1]++;
-				mx = max(mx , i - C[i] + 1);
-			}
-		}
-	}
-	
-	dp[0] = 1;
-	cout<<mx<<" "<<dp[mx];
+typedef vector<int> vi;
+int mod=10e9+7;
+#define deb(v) for(auto x: v){cout<<x<<" ";}cout<<"\n";
+#define deb2(v) for(auto x: v){cout<<x.first<<","<<x.second<<" ";}cout<<"\n";
+struct hash_pair {
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1 = hash<T1>{}(p.first);
+        auto hash2 = hash<T2>{}(p.second);
+        return hash1 ^ hash2;
+    }
+};
+struct  Data {
+  int min;
+  int max;
+  int sum;
+  Data(int min=0, int max=0, int sum=0) : min(min), max(max), sum(sum) {};
+};
+bool bysec(const pair<int,int> &a, const pair<int,int> &b){
+    if(a.second==b.second){return (a.first > b.first);}
+    return a.second < b.second;
+}
+void solve(){
+  string s;cin>>s;
+  int n=s.size();
+  // vi lastOpen(n), currOpen(n);
+  stack<int> st;
+  st.push(-1);
+  unordered_map<int, int> count;
+  int result=0;
+  for(int i=0;i<n;i++){
+    if(s[i]=='('){
+      st.push(i);
+    }
+    else{
+      if(!st.empty()){
+        st.pop();
+      }
+      if( !st.empty() ){
+        result = max( result, i - st.top() );
+        count[i - st.top()]++;
+      }
+      else st.push(i);
+    }
+  }
+  cout<<result<<" "<<max( count[result], 1ll);
+  return ;
+  auto Life=nullptr;
+  cout<<(&Life);
+}
+int32_t main(){
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+  #endif
+  int TC=1;//cin>>TC;
+  while(TC--)solve();
 }
