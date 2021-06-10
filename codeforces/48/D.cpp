eae4305f6fbf1b628096e90e7673c90d6254d28e
Sahil Amin <sahilamin219@gmail.com>
2021-06-10 18:53:00 +0530

    Add solution for problem `D - Permutations` Link: https://codeforces.com/contest/48/problem/D Tags: greedy, *1500 Ref: https://codeforces.com/contest/48/submission/118967515

--- codeforces/48/D.cpp
+++ codeforces/48/D.cpp
@@ -96,3 +96,44 @@ int32_t main(){
 	while(TestCase--)Solve_cf();
 	return 0;
 }
+
+const int MAX=10000007;
+int prime[MAX]; 
+int prime_count[MAX]; 
+void sieve() 
+{ 
+    memset(prime, 0, sizeof(prime)); 
+    prime[0] = prime[1] = 1; 
+    for (int i = 2; i * i < MAX; i++) { 
+        if (prime[i] == 0) { 
+            for (int j = i * 2; j < MAX; j += i) { 
+                if (prime[j] == 0) 
+                    prime[j] = i; 
+            } 
+        } 
+    } 
+    for (int i = 2; i < MAX; i++) { 
+        // If the number is prime then it's  
+        // smallest prime factor is the number itself 
+        if (prime[i] == 0) 
+            prime[i] = i; 
+    } 
+} 
+long long numberOfDivisorsOfProduct(vector<int> arr,int n) 
+{ 
+    memset(prime_count, 0, sizeof(prime_count)); 
+    for (int i = 0; i < n; i++) { 
+        int temp = arr[i]; 
+        while (temp != 1) { 
+            // Increase the count of prime encountered 
+            prime_count[prime[temp]]++; 
+            temp = temp / prime[temp]; 
+        } 
+    } 
+    long long ans = 1; 
+    // Multiplying the count of primes encountered 
+    for (int i = 2; i < MAX; i++) { 
+        ans = ((ans%mod)*(prime_count[i] + 1))%mod; 
+    } 
+    return ans%mod; 
+} 
