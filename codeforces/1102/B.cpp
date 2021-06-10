703fab5aa06e68af4b23641f9bfeab737d431a43
Sahil Amin <sahilamin219@gmail.com>
2021-06-10 17:52:00 +0530

    Add solution for problem `B - Array K-Coloring` Link: https://codeforces.com/contest/1102/problem/B Tags: greedy, sortings, *1400 Ref: https://codeforces.com/contest/1102/submission/118962324

--- codeforces/1102/B.cpp
+++ codeforces/1102/B.cpp
@@ -93,6 +93,10 @@ void Solve_cf(){
 
 int32_t main(){
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	Solve_cf();
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+	#endif
+	int TestCase=1;//cin>>TestCase;
+	while(TestCase--)Solve_cf();
 	return 0;
 }
