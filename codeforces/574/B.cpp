d08a93b320281f4ca31c4f97ccb5432b28a794e2
Sahil Amin <sahilamin219@gmail.com>
2021-06-25 15:05:00 +0530

    Add solution for problem `B - Bear and Three Musketeers` Link: https://codeforces.com/contest/574/problem/B Tags: brute force, dfs and similar, graphs, hashing, *1500 Ref: https://codeforces.com/contest/574/submission/120509163

---
+++ codeforces/574/B.cpp
@@ -0,0 +1,33 @@
+// AUTHOR :: SAHIL - AMIN
+#include "bits/stdc++.h"
+using namespace std;
+bool u[5005][5005];
+void solve(){
+	int n, m;cin>>n>>m;
+	unordered_map<int, int> degree;
+	for(int i=0, a, b;i<m;i++){
+		cin>>a>>b;
+		u[a][b]=u[b][a]=true;
+		degree[a]++;
+		degree[b]++;
+	}
+	int ans=INT_MAX;
+	for(int i=1;i<=n;i++){
+		for(int j=i+1;j<=n;j++){
+			if(u[i][j]){
+				for(int k=j+1;k<=n;k++){
+					if( u[j][k] and u[i][k]){
+						ans=min(ans, degree[i]+degree[j]+degree[k]);
+					}
+				}
+			}
+		}
+	}
+	if(ans == INT_MAX) puts("-1");
+	else printf("%d\n", ans - 6);
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    solve();
+}
