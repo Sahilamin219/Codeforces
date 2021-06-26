6ff9e8b3ad4b4f2a11fb4a2a1ab11ac7e717ac5b
Sahil Amin <sahilamin219@gmail.com>
2021-06-26 23:25:00 +0530

    Add solution for problem `D - Palindrome pairs` Link: https://codeforces.com/contest/159/problem/D Tags: *special problem, brute force, dp, strings, *1500 Ref: https://codeforces.com/contest/159/submission/120689474

--- codeforces/159/D.cpp
+++ codeforces/159/D.cpp
@@ -100,6 +100,25 @@ public:
 	return dp[s.size()][p.size()] = finding(s, p, s.size() - 1, p.size() - 1);
 	}
 };
+// In-place rotate it by 90 degrees in a clockwise direction
+// void rotate(int mat[N][N])
+// {
+//     // Transpose the matrix
+//     for (int i = 0; i < N; i++)
+//     {
+//         for (int j = 0; j < i; j++) {
+//             swap(mat[i][j], mat[j][i]);
+//         }
+//     }
+ 
+//     // swap columns
+//     for (int i = 0; i < N; i++)
+//     {
+//         for (int j = 0; j < N/2; j++) {
+//             swap(mat[i][j], mat[i][N - j - 1]);
+//         }
+//     }
+// }
 int minSwaps(vector<int>&nums)
 {
     map<int,int>m;
@@ -160,6 +179,90 @@ void solve(){
 
 int32_t main(){
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	solve();
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+	#endif
+	int t=1;//cin>>t;
+	while(t--)solve();
 	return 0;
 }
+// long int stol (const string&  str, size_t* idx = 0, int base = 10)
+// result = std::accumulate(arr, arr + size, num, std::minus<int>()); 
+// int equal(vector<int> arr) {
+//	 auto min_ = *min_element(arr.begin(), arr.end());
+//	 auto ans = numeric_limits<int>:: max();
+//	 for(auto i=0;i<5;i++){
+//		 auto operations = [min_,i](auto init, auto j){
+//			 auto t = j-(min_-i);
+//			 return init + t/5 + t%5/2 + t%5%2;
+//		 };
+//		 ans=min(ans,accumulate(arr.begin(), arr.end(), 0, operations));
+//	 }
+//	 return ans;
+// }
+// pair<long long,int> dfs(int node)
+// {
+// 	long long mx=0;
+// 	int sz=1;
+// 	for (int u:v[node])
+// 	{
+// 		auto tmp=dfs(u);
+// 		mx=max(mx,tmp.first);
+// 		sz+=tmp.second;
+// 	}
+// 	return {mx+sz,sz};
+// }
+// function<void(int,int)>dfs=[&](int i, int d){
+// 	if(i==b){
+// 		dab=d;return;
+// 	}
+// 	visited[i]=true;
+// 	for(auto it:adj[i]){
+// 		if(!visited[it])
+// 			dfs(it,d+1);
+// 	}
+// };
+// dfs(a,0);
+// int dot=1;
+// vector<bool> v(n+1,false);
+// function<int(int)>diameter=[&](int i){
+// 	int m1=0,m2=0;
+// 	v[i]=true;
+// 	for(auto it:adj[i]){
+// 		if(!v[it]){
+// 			int k=diameter(it);
+// 			if(k>m1){
+// 				m2=m1;
+// 				m1=k;
+// 			}
+// 			else if(k>m2)
+// 				m2=k;
+// 		}
+// 	}
+// 	dot=max(m1+m2,dot);
+// 	return 1+m1;
+// };
+
+void saifu(int node, int p)
+{
+	vector<int> others;
+	int n=10, sub=1;
+	int dp[10], dp2[10];
+    if(others.max_size()>0){
+	    if(p!=-1) others.push_back(n-sub);
+	    for(int i=1;i<=n;i++) dp2[i] = 0;
+	    dp2[0] = 1;
+	    for(int each : others){
+	        for(int i=n-each;i>=0;i--){
+	            if(dp2[i]==1) dp2[i+each]=1,dp[i+each]=1;
+	        }
+	    }
+	}
+}
+
+// Given a connect Directed Graph,
+// find the minimum number of edge required
+// to make every vertex reachable from each other,
+// Important
+// the condition given that you can only add edge u to v if there is an edge from v to u already exist.
+
